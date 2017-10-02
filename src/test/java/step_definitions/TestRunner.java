48a2b58ffe9cf65cdfb6a4add605c04e708ce6b5
Degwy <degwy@github.com>
2017-10-02 22:55:28 +0200

    Changing the place of feature file

--- src/test/java/step_definitions/TestRunner.java
+++ src/test/java/step_definitions/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions (features = "src/test/java/features/", plugin = { "html:target/cucumber-html-report", "json:target/cucumber.json",
+@CucumberOptions (features = "Features/", plugin = { "html:target/cucumber-html-report", "json:target/cucumber.json",
         "pretty:target/cucumber-pretty.txt", "usage:target/cucumber-usage.json",
         "junit:target/cucumber-results.xml" })
 public class TestRunner {
